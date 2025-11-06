a5aaa07c5ebd064b2913c2ecddea82df03bc1800
BaPM <phuongmanhba@gmail.com>
2025-11-06 11:43:34 +0700

    Update trigonometry.h

---
+++ trigonometry.h
@@ -0,0 +1,12 @@
+
+#ifndef TRIGONOMETRY_H
+#define TRIGONOMETRY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Hàm hiển thị menu lượng giác
+void menuTrigonometry(void);
+
+#endif // TRIGONOMETRY_H
